b00b1f43195635d2d8f92bc0482d9d64a1f13346
shiwoo.park <87423085+custardcream98@users.noreply.github.com>
2022-11-12 15:13:06 +0900

    Refactoring: Post 모델 유효성 검증 순서 변경

--- functions/src/models/post.model.js
+++ functions/src/models/post.model.js
@@ -3,6 +3,9 @@ const { tagsList } = require("../constants/tagsList.constant");
 
 class Post {
   constructor({ uid, title, subject, tags, content }) {
+    if (!uid || !title || !subject || tags.length === 0 || !content)
+      throw Error("[포스트 Post Request Error] 포스트 데이터에 모든 값을 넣어주세요.");
+
     if (!subjectList.includes(subject))
       throw Error("[포스트 Post Request Error] 주제가 잘못 입력됐습니다.");
 
@@ -10,9 +13,6 @@ class Post {
       if (!tagsList.includes(tag))
         throw Error("[포스트 Post Request Error] 태그가 잘못 입력됐습니다.");
     });
-
-    if (!uid || !title || !subject || tags.length === 0 || !content)
-      throw Error("[포스트 Post Request Error] 포스트 데이터에 모든 값을 넣어주세요.");
 
     this.uid = uid;
     this.title = title;
